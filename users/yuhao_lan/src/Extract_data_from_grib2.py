a09eb4ecbfa31cefcefcf823c36f7021b07736cd
https://www.linkedin.com/in/yuhao-lan-71b457109 <lanyuhao@nmsu.edu>
2017-11-09 17:06:41 -0700

    Add files via upload

--- users/yuhao_lan/src/Extract_data_from_grib2.py
+++ users/yuhao_lan/src/Extract_data_from_grib2.py
@@ -5,6 +5,7 @@ import sys
 import tarfile
 from os import listdir
 import logging
+import re
 
 def unzip_tarfile(tar_file):
 	#extract all files
@@ -53,7 +54,7 @@ def read_one_grib2(file,longitude,latitude, logger):
 		nearest_col_index = y[0]
 		#get 317 layers of of nearest cell data
 
-		#get the size of grib2
+		#get the number of layers(size) of grib2
 		if i == 0:
 			size = 0
 			for layer in grbs:
@@ -61,20 +62,23 @@ def read_one_grib2(file,longitude,latitude, logger):
 		#print "layer number " + str(size)
 		logger.info('layer number :%s ', str(size))
 		
-		size = 317
 		one_instance = get_all_layers_data(grbs, nearest_row_index, nearest_col_index, size, logger)
+
+		eight_non_dust_event_instance = get_8_non_dust_event_data(grbs, nearest_row_index, nearest_col_index, size, logger)
 		#add target longitude, target latitude, actual longitude, actual latitude to the top of the instance array
 		one_instance = np.insert(one_instance,0,[t_long, t_lat,all_lons[x[0],y[0]], all_lats[x[0],y[0]] ])
-
+		eight_non_dust_event_instance = np.insert(eight_non_dust_event_instance,0,[t_long, t_lat,all_lons[x[0],y[0]], all_lats[x[0],y[0]] ])
 		if i == 0:
 			all_instance_of_one_grib2 = one_instance
+			all_8_none_dust_cells_of_one_grib2 = eight_non_dust_event_instance
 		else:
 			# add each instance to the bottom 
 			all_instance_of_one_grib2 = np.vstack( (all_instance_of_one_grib2,one_instance) )
+			all_8_none_dust_cells_of_one_grib2 = np.vstack( (all_8_none_dust_cells_of_one_grib2,eight_non_dust_event_instance) )
 
 
 
-	return all_instance_of_one_grib2
+	return all_instance_of_one_grib2, all_8_none_dust_cells_of_one_grib2
 	
 
 #find the cell's values of all layers in one grbs file
@@ -83,11 +87,84 @@ def get_all_layers_data(grbs, nearest_row_index, nearest_col_index, size, logger
 	#initialize to be zero 
 	row_values = np.asarray([])
 	for i in range(1,size+1):
+		if i == 5:
+			break
 		target_value = grbs[i].values[nearest_row_index,nearest_col_index]
 		row_values = np.append(row_values,target_value)
 		#print "finished capturing layer " + str(i)
 		logger.info('finished capturing layer %s', str(i))
+
 	return row_values
+
+#find the 8 cells that are arounding the target cells, and return all the instances
+def get_8_non_dust_event_data(grbs, nearest_row_index, nearest_col_index, size, logger):
+	#row_values is one instance contains 317 layers value
+	#initialize to be zero 
+	cell1_values = np.asarray([])
+	cell2_values = np.asarray([])
+	cell3_values = np.asarray([])
+	cell4_values = np.asarray([])
+	cell5_values = np.asarray([])
+	cell6_values = np.asarray([])
+	cell7_values = np.asarray([])
+	cell8_values = np.asarray([])
+	all_8_none_dust_cells =  np.asarray([])
+	# shape of each layer (337, 451), index max (336,450)
+	for i in range(1,size+1):
+
+		if i == 10:
+			break
+		
+		if nearest_row_index + 1 < 337:
+			cell1_values = np.append(cell1_values,grbs[i].values[nearest_row_index + 1,nearest_col_index])
+
+		if nearest_row_index + 1 < 337 and nearest_col_index + 1 < 451:
+			cell2_values = np.append(cell2_values,grbs[i].values[nearest_row_index + 1,nearest_col_index + 1])
+
+		if nearest_row_index + 1 < 337 and nearest_col_index - 1 > 0:
+			cell3_values = np.append(cell3_values,grbs[i].values[nearest_row_index + 1,nearest_col_index - 1])
+
+		if nearest_row_index - 1 > 0:
+			cell4_values = np.append(cell4_values,grbs[i].values[nearest_row_index - 1,nearest_col_index])
+
+		if nearest_row_index - 1 > 0 and nearest_col_index + 1 < 451:
+			cell5_values = np.append(cell5_values,grbs[i].values[nearest_row_index - 1,nearest_col_index + 1])
+
+		if nearest_row_index - 1 > 0 and nearest_col_index - 1 > 0:
+			cell6_values = np.append(cell6_values,grbs[i].values[nearest_row_index - 1,nearest_col_index - 1])
+
+		if  nearest_col_index - 1 > 0:
+			cell7_values = np.append(cell7_values,grbs[i].values[nearest_row_index,nearest_col_index - 1])
+
+		if  nearest_col_index + 1 < 451:
+			cell8_values = np.append(cell8_values,grbs[i].values[nearest_row_index,nearest_col_index + 1])
+		#target_value = grbs[i].values[nearest_row_index,nearest_col_index]
+		#row_values = np.append(row_values,target_value)
+		#print "finished capturing layer " + str(i)
+		logger.info('finished capturing 8_non_dust layer %s', str(i))
+
+		if cell1_values.size != 0:
+			all_8_none_dust_cells = cell1_values
+		if cell2_values.size != 0:
+			all_8_none_dust_cells = np.vstack((all_8_none_dust_cells,cell2_values))
+		if cell3_values.size != 0:
+			all_8_none_dust_cells = np.vstack((all_8_none_dust_cells,cell3_values))
+		if cell4_values.size != 0:
+			all_8_none_dust_cells = np.vstack((all_8_none_dust_cells,cell4_values))
+		if cell5_values.size != 0:
+			all_8_none_dust_cells = np.vstack((all_8_none_dust_cells,cell5_values))
+		if cell6_values.size != 0:
+			all_8_none_dust_cells = np.vstack((all_8_none_dust_cells,cell6_values))
+		if cell7_values.size != 0:
+			all_8_none_dust_cells = np.vstack((all_8_none_dust_cells,cell7_values))
+		if cell8_values.size != 0:
+			all_8_none_dust_cells = np.vstack((all_8_none_dust_cells,cell8_values))
+
+
+	return all_8_none_dust_cells
+
+
+
 
 
 #read all grib2 files in one folder
@@ -110,6 +187,10 @@ def read_all_grib2(file_folder, hour_start, hour_end, logger):
 		return
 	#read all file in one folder
 	i = 0
+	#don't read all f01.grib2 files
+	delete_file = "f01.grib2"
+	first = True
+	second = False
 	for grib2 in all_files_name:
 		#print "reading grib2 file: " + str(grib2)
 		logger.info('reading grib2 file: %s', str(grib2))
@@ -117,16 +198,46 @@ def read_all_grib2(file_folder, hour_start, hour_end, logger):
 			#print "pass"
 			logger.info('pass')
 			continue
-		#target longitude, target latitude, actual longitude, actual latitude, 317 layers values
-		all_instance_of_one_grib2 = read_one_grib2( './' + file_folder + '/' + grib2,Longitude,Latitude, logger)
+
+		if delete_file in grib2:
+			logger.info('pass')
+			continue
+
+
+		if i % 2 == 0:
+		#target longitude, target latitude, actual longitude, actual latitude, 750 layers values
+			first_grib, first_8_nondust = read_one_grib2( './' + file_folder + '/' + grib2,Longitude,Latitude, logger)
+			print first_8_nondust
+		#each odd time, concatenate the files
+		if i % 2 == 1:
+			second_grib, second_8_nondust = read_one_grib2( './' + file_folder + '/' + grib2,Longitude,Latitude, logger)
+
+			#check the dim of numpy array second grib
+			if second_grib.ndim == 1:
+			#delete first 4 columns, because they are target longitude, target latitude, actual longitude, actual latitude
+				second_grib = np.delete(second_grib, [0,1,2,3],axis = 0)
+				all_instance_of_one_grib2 = np.append(first_grib,second_grib,axis = 0)
+			else:
+			#delete first 4 columns, because they are target longitude, target latitude, actual longitude, actual latitude
+				second_grib = np.delete(second_grib, [0,1,2,3], 1)
+				all_instance_of_one_grib2 = np.append(first_grib,second_grib,axis = 1)
+
+			#produce nondust instances
+			print second_8_nondust
+			second_8_nondust = np.delete(second_8_nondust, [0,1,2,3], 1)
+			eight_nondust_of_one_instance = np.append(first_8_nondust,second_8_nondust,axis = 1)
+
 		#only one file
-		if i == 0:
+		if i == 1:
 			all_instance_of_all_grib2 = all_instance_of_one_grib2
-		else:
+			all_instance_of_non_dust = eight_nondust_of_one_instance
+		if i % 2 == 1 and i > 1:
 			# add each instance to the bottom 
 			all_instance_of_all_grib2 = np.vstack( (all_instance_of_all_grib2,all_instance_of_one_grib2) )
+			all_instance_of_non_dust = np.vstack( (all_instance_of_non_dust,eight_nondust_of_one_instance) )
 		i = i + 1
-
+		if i == 10:
+			break
 
 	#print all_instance_of_all_grib2
 
@@ -134,8 +245,19 @@ def read_all_grib2(file_folder, hour_start, hour_end, logger):
 	all_data.insert(0,'hour_end', hour_end)
 	all_data.insert(0,'hour_start',hour_start)
 	all_data.insert(0,'date',date)
-	all_data.to_csv(str(file_folder) + str(hour_start) + '_' + str(hour_end) + '.csv')
-	logger.info('finished writing %s', str(str(file_folder) + str(hour_start) + '_' + str(hour_end) + '.csv') )
+	all_data.to_csv(str(file_folder) + str(hour_start) + '_' + str(hour_end) + 'csv')
+	logger.info('finished writing %s', str(str(file_folder) + str(hour_start) + '_' + str(hour_end) + 'csv') )
+
+
+	all_non_dust = pd.DataFrame(all_instance_of_non_dust)
+	all_non_dust.insert(0,'hour_end', hour_end)
+	all_non_dust.insert(0,'hour_start',hour_start)
+	all_non_dust.insert(0,'date',date)
+	all_non_dust.to_csv(str(file_folder) + str(hour_start) + '_' + str(hour_end) + 'non_dust.csv')
+	logger.info('finished writing %s', str(str(file_folder) + str(hour_start) + '_' + str(hour_end) + 'non_dust.csv') )
+
+
+
 
 
 def main():
